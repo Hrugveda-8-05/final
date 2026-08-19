439d183f2229f77e321c6aad68d46a0c60dca725
Hrugveda-8-05 <23104040@apsit.edu.in>
2026-08-19 13:21:23 +0530

    Update a.py

--- a.py
+++ a.py
@@ -22,28 +22,4 @@ def run_command(user_input):
 SECRET_KEY = "hardcoded_super_secret_123"
 AWS_KEY = "AKIAIOSFODNN7EXAMPLE"
 
-# 4. Insecure Deserialization
-def load_data(raw_bytes):
-    return pickle.loads(raw_bytes)
 
-# 5. Path Traversal
-def read_file(filename):
-    base_dir = "/var/www/uploads/"
-    path = base_dir + filename
-    with open(path, "r") as f:
-        return f.read()
-
-# 6. Weak Hashing (MD5)
-def hash_password(password):
-    return hashlib.md5(password.encode()).hexdigest()
-
-# 7. SQL Injection (parameterized missing)
-def delete_user(user_id):
-    conn = sqlite3.connect("users.db")
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM users WHERE id = " + str(user_id))
-    conn.commit()
-
-# 8. OS command with user input
-def ping_host(host):
-    os.system("ping -c 1 " + host)
